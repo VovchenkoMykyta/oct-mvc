c4d69e75c8fca6fe2ffeff497f807c52fd1a264b
VovchenkoMykyta <vovchenkonikita36@gmail.com>
2021-06-30 14:10:10 +0300

    modified files 002

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -5,11 +5,13 @@ namespace controllers;
 
 
 use core\BaseController;
+use core\View;
 
 class IndexController extends BaseController
 {
     public function index()
     {
-        echo 'Index index';
+        $view = new View();
+        $view->render('index_index_view.php', 'default_view.php');
     }
 }
--- app/core/View.php
+++ app/core/View.php
@@ -6,5 +6,8 @@ namespace core;
 
 class View
 {
-
+    public function render($pageView, $templateView)
+    {
+        include_once 'views' . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR . $templateView;
+    }
 }
