2ff71e3274157c3e036b6640765e5bb127b8b88a
tburakova <tatyana.burakova.97@gmail.com>
2021-07-01 21:02:21 +0300

    textarea instead input

--- app/views/task_edit_view.php
+++ app/views/task_edit_view.php
@@ -1,7 +1,7 @@
 <h2>Edit task</h2>
 <form method="post" action="<?=\core\Route::getUrl('task', 'edit')?>">
     <label> Task name
-        <input type="text" name="name" value="">
+        <textarea cols="30" rows="10"></textarea>
     </label>
     <input type="submit" value="edit">
 </form>
