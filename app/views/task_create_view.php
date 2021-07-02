55d2ac1d2d9054b4e208fd3da9e2715cff04e4f7
VovchenkoMykyta <vovchenkonikita36@gmail.com>
2021-07-02 15:36:52 +0300

    add styles

--- app/views/task_create_view.php
+++ app/views/task_create_view.php
@@ -1,7 +1,7 @@
 <h2>Create new task</h2>
 <form method="post" action="<?=\core\Route::getUrl('task', 'store')?>">
     <label> Task name
-        <input type="text" name="name">
+        <input type="text" name="name" placeholder="Enter task name">
     </label>
     <input type="submit" value="create">
 </form>
