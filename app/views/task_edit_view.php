4bd7b54ebca77cdad56b3ce9e5186794fc36f178
tburakova <tatyana.burakova.97@gmail.com>
2021-07-06 17:38:41 +0300

    modified styles

--- app/views/task_edit_view.php
+++ app/views/task_edit_view.php
@@ -1,7 +1,7 @@
 <h2>Edit task</h2>
 <form method="post" action="<?=\core\Route::getUrl('task', 'edit')?>">
     <label> Task name
-        <input type="text" name="name" value="<?= $_POST['name'] ?>">
+        <input type="text" name="name" value="<?= $_POST['name'] ?>" id="edit-input">
         <input type="hidden" name="id" value="<?= $_POST['id'] ?>">
 <!--        <textarea></textarea>-->
     </label>
