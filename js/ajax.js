4ba502a42944952f6245a4c12dd322af10c85505
tburakova <tatyana.burakova.97@gmail.com>
2021-07-08 18:12:40 +0300

    add ajax.js

---
+++ js/ajax.js
@@ -0,0 +1,13 @@
+function getAllTasks(){
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', '/api/getalltasks');
+    xhr.onreadystatechange = function () {
+        if(xhr.readyState === 4){
+            if(xhr.status === 200){
+                alert('we get response')
+            }else{
+                console.error('some problems with get all tasks')
+            }
+        }
+    };
+}
